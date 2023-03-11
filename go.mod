fb771ba6a91ddc6ccc935c420f7e525e4b8e5a43
Kyour.cn <kyour@vip.qq.com>
2023-03-11 15:31:43 +0800

    [perf] 完善代码

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module mysql
 
 go 1.20
+
+require (
+	github.com/go-gourd/gourd v0.2.1
+	gorm.io/gorm v1.24.6
+)
+
+//本地开发框架包替换
+replace github.com/go-gourd/gourd => ../gourd
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
--- mysql.go
+++ mysql.go
@@ -1,5 +1,71 @@
 package mysql
 
-func GetDb() {
+import (
+	"errors"
+	"fmt"
+	"github.com/go-gourd/gourd/config"
+	"github.com/go-gourd/gourd/logger"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	gormLogger "gorm.io/gorm/logger"
+	"time"
+)
 
+var dbs = make(map[string]*gorm.DB)
+
+type LogWriter struct{}
+
+func (w LogWriter) Printf(format string, args ...any) {
+	logger.Errorf(format, args...)
+}
+
+// GetDb 获取数据库连接
+func GetDb(name string) (*gorm.DB, error) {
+
+	if _, ok := dbs[name]; ok {
+		return dbs[name], nil
+	}
+
+	dbConf := config.GetDbConfig()
+	if _, ok := dbConf[name]; !ok {
+		return nil, errors.New("Database config '" + name + "' does not exist.")
+	}
+
+	conf := dbConf[name]
+
+	dsnParam := ""
+	if conf.Param != "" {
+		dsnParam = "?" + conf.Param
+	}
+	dsnF := "%s:%s@(%s:%d)/%s%s"
+	dsn := fmt.Sprintf(dsnF, conf.User, conf.Pass, conf.Host, conf.Port, conf.Database, dsnParam)
+
+	// 慢日志阈值
+	slowLogTime := conf.SlowLogTime
+	if slowLogTime == 0 {
+		slowLogTime = 60000 //默认1分钟
+	}
+
+	newLogger := gormLogger.New(
+		LogWriter{},
+		gormLogger.Config{
+			SlowThreshold:             time.Duration(slowLogTime) * time.Millisecond, // 慢 SQL 阈值
+			LogLevel:                  gormLogger.Warn,                               // 日志级别
+			IgnoreRecordNotFoundError: true,                                          // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  false,                                         // 禁用彩色打印
+		},
+	)
+
+	// 连接数据库
+	newDb, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		logger.Error("cannot establish db connection: %w" + err.Error())
+		return nil, err
+	}
+
+	dbs[name] = newDb
+
+	return newDb, nil
 }
